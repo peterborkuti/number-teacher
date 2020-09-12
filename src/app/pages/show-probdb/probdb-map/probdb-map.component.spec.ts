62436886c5568386e400402e6ccef178abe300d6
Peter Borkuti <peterborkuti@gmail.com>
2020-09-12 09:31:52 +0200

    probdb-map test

--- src/app/pages/show-probdb/probdb-map/probdb-map.component.spec.ts
+++ src/app/pages/show-probdb/probdb-map/probdb-map.component.spec.ts
@@ -21,4 +21,17 @@ describe('ProbdbMapComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('shows db values', () => {
+    const expected = Array(10).fill(1).map( i => Math.floor(Math.random()* 100));
+    component.probabilities = [expected];
+    fixture.detectChanges();
+
+    const table: HTMLTableElement = fixture.nativeElement.querySelector('table');
+    const row: HTMLTableRowElement = table.rows[1];
+    const cells: HTMLCollectionOf<HTMLTableDataCellElement> = row.cells;
+
+    expect(cells[0].innerText).toBe("0");
+    expected.forEach((e,i) => expect(cells[i + 1].innerText).toBe('' + e));
+  })
 });
