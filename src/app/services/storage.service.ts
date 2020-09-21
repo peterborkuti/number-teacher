996a2092c5eeeca57600cb0b26ee7e644c1754f2
Peter Borkuti <peterborkuti@gmail.com>
2020-09-21 11:48:11 +0200

    storage service

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { ProbDB } from './core/prob-db';
 import { Storage } from '@ionic/storage';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 
 })
 export class StorageService {
+  private readonly KEY_ACTIVE = 'ACTIVE_';
+  private readonly KEY_DB = 'DB_';
   
   private probdbs: ProbDB[] = [];
   private active: ProbDB;
 
+  public $storageIsReady = new BehaviorSubject<boolean>(false);
+
   constructor(private storage: Storage) {
-    //this.probdbs = this.loadAll();
+    this.loadAll();
   }
 
   reset(nameOfDB: string, probabilities: number[][]) {
     const db = this.getOrCreate(nameOfDB);
     db.probabilities = JSON.parse(JSON.stringify(probabilities));
+    this.save(db);
   }
 
   private getOrCreate(nameOfDB = 'Default'): ProbDB {
@@ -32,7 +38,9 @@ export class StorageService {
   }
 
   setActive(nameOfDB: string) {
-    this.active = this.getOrCreate(nameOfDB);
+      this.active = this.getOrCreate(nameOfDB);
+      this.saveActiveName(this.active.name);
+
   }
 
   getName(): string {
@@ -44,17 +52,16 @@ export class StorageService {
   }
 
   getProbabilities(): number[][] {
-    // deep copy
-    return JSON.parse(JSON.stringify(this.active.probabilities));
+    return (this.active && this.active.probabilities) ? JSON.parse(JSON.stringify(this.active.probabilities)) : [[]];
   }
 
   setProb(exp: number, digit: number, probability: number) {
     if (!this.existsProb(exp, digit)) {
-      console.error(`Digit:${digit} or exponent:${exp} out of range`);
       return;
     }
 
     this.active.probabilities[exp][digit] = probability;
+    this.save(this.active);
   }
 
   private existsProb(exp: number, digit: number): boolean {
@@ -62,7 +69,7 @@ export class StorageService {
   }
 
   getProb(exp: number, digit: number): number {
-    return this.active.probabilities[exp][digit]
+    return this.existsProb(exp, digit) ? this.active.probabilities[exp][digit] : 0;
   }
 
 
@@ -76,17 +83,27 @@ export class StorageService {
     return dbs.length === 0 ? undefined : dbs[0];
   }
 
+  private saveActiveName(name: string) {
+    this.storage.set(this.KEY_ACTIVE, name);
+  }
+
   private save(probDB: ProbDB) {
-
+    this.storage.set(this.KEY_DB + probDB.name, probDB);
   }
 
-  private load(nameOfDB: string): ProbDB {
-    return null;
+  private loadAll() {
+    let activeName = '';
 
-  }
-
-  private loadAll(): ProbDB[] {
-    return []
-
+    this.storage.forEach((value, key) => {
+      if (key == this.KEY_ACTIVE) {
+        activeName = value;
+      }
+      else {
+        this.probdbs.push(value)
+      }})
+    .then(() => {
+      this.active = this.getDB(activeName);
+      this.$storageIsReady.next(true)
+    });
   }
 }
