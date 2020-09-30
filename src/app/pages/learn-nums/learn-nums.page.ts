d33886f63238da2cf764e9f7c732bf642a08ce21
Peter Borkuti <peterborkuti@gmail.com>
2020-09-30 07:52:03 +0200

    learn page reflects changes in probd immediately

--- src/app/pages/learn-nums/learn-nums.page.ts
+++ src/app/pages/learn-nums/learn-nums.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProbdbService } from 'src/app/services/core/probdb.service';
 
 @Component({
   selector: 'app-learn-nums',
   templateUrl: './learn-nums.page.html',
   styleUrls: ['./learn-nums.page.scss'],
 })
-export class LearnNumsPage implements OnInit {
+export class LearnNumsPage implements OnInit, OnDestroy {
+  reset = 0;
+  groupName = '';
 
-  constructor() { }
+  private subscription: Subscription;
+
+  constructor(private probdbService: ProbdbService) {
+    this.subscription = this.probdbService.$ready.subscribe(ready => ready && this.init());
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  init() {
+    this.groupName = this.probdbService.getName();
+    this.reset = this.reset % 2 + 1;
+  }
+
+  ionViewDidEnter() {
+    if (this.probdbService.ready) {
+      this.init();
+    }
   }
 
 }
